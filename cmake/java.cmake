46d1aabfee7166108a5305a00d10fd5b745e25ad
RIscRIpt <richard@riscript.com>
2020-09-03 21:31:54 +0300

    Create CMakeLists with support of JNI

---
+++ cmake/java.cmake
@@ -0,0 +1,25 @@
+set(JAVA_HOME $ENV{JAVA_HOME})
+
+find_package(Java 14.0 COMPONENTS Development REQUIRED)
+find_package(JNI 14.0 REQUIRED)
+
+function(add_jni_dll target)
+    cmake_parse_arguments("" "" "PATH" "JAVA_SOURCES;NATIVE_SOURCES" ${ARGN})
+    set(input "${CMAKE_SOURCE_DIR}/${_PATH}")
+    set(output "${CMAKE_BINARY_DIR}/${_PATH}")
+    set(java_src ${_JAVA_SOURCES})
+    set(native_src ${_NATIVE_SOURCES})
+    list(TRANSFORM java_src PREPEND "${output}/")
+    list(TRANSFORM native_src PREPEND "${output}/")
+    add_custom_target(${target}
+        COMMAND "${CMAKE_COMMAND}" -E copy_directory "${input}" "${output}"
+        COMMAND "${Java_JAVAC_EXECUTABLE}" ${java_src}
+        COMMAND "${Java_JAVAC_EXECUTABLE}" -h ${output} ${java_src}
+    )
+    add_library("${target}_dll" SHARED ${native_src})
+    target_include_directories("${target}_dll" SYSTEM PRIVATE ${JNI_INCLUDE_DIRS})
+    target_include_directories("${target}_dll" PRIVATE ${CMAKE_SOURCE_DIR})
+    target_link_libraries("${target}_dll" imagequant ${JNI_LIBRARIES})
+    set_property(SOURCE ${native_src} PROPERTY GENERATED 1)
+    add_dependencies("${target}_dll" "${target}")
+endfunction()
