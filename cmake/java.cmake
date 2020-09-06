ed1a5974c3108e1b7d46cf0ecb9a51c16e4aff88
RIscRIpt <richard@riscript.com>
2020-09-06 19:17:41 +0300

    Update CMake, add support for static lib

--- cmake/java.cmake
+++ cmake/java.cmake
@@ -3,7 +3,7 @@ set(JAVA_HOME $ENV{JAVA_HOME})
 find_package(Java 14.0 COMPONENTS Development REQUIRED)
 find_package(JNI 14.0 REQUIRED)
 
-function(add_jni_dll target)
+function(add_jni target)
     cmake_parse_arguments("" "" "PATH" "JAVA_SOURCES;NATIVE_SOURCES" ${ARGN})
     set(input "${CMAKE_SOURCE_DIR}/${_PATH}")
     set(output "${CMAKE_BINARY_DIR}/${_PATH}")
@@ -11,15 +11,15 @@ function(add_jni_dll target)
     set(native_src ${_NATIVE_SOURCES})
     list(TRANSFORM java_src PREPEND "${output}/")
     list(TRANSFORM native_src PREPEND "${output}/")
-    add_custom_target(${target}
+    add_custom_target("${target}_headers"
         COMMAND "${CMAKE_COMMAND}" -E copy_directory "${input}" "${output}"
         COMMAND "${Java_JAVAC_EXECUTABLE}" ${java_src}
         COMMAND "${Java_JAVAC_EXECUTABLE}" -h ${output} ${java_src}
     )
-    add_library("${target}_dll" SHARED ${native_src})
-    target_include_directories("${target}_dll" SYSTEM PRIVATE ${JNI_INCLUDE_DIRS})
-    target_include_directories("${target}_dll" PRIVATE ${CMAKE_SOURCE_DIR})
-    target_link_libraries("${target}_dll" imagequant ${JNI_LIBRARIES})
+    add_library("${target}" SHARED ${native_src})
+    target_include_directories("${target}" SYSTEM PRIVATE ${JNI_INCLUDE_DIRS})
+    target_include_directories("${target}" PRIVATE ${CMAKE_SOURCE_DIR})
+    target_link_libraries("${target}" imagequant ${JNI_LIBRARIES})
     set_property(SOURCE ${native_src} PROPERTY GENERATED 1)
-    add_dependencies("${target}_dll" "${target}")
+    add_dependencies("${target}" "${target}_headers")
 endfunction()
